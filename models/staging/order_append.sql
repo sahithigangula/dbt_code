2dad302826c8edffd3eb56ba730bd0003f890465
sahithi g <sahithinaresh1330@gmail.com>
2024-07-06 13:15:34 +0000

    commit

--- models/staging/order_append.sql
+++ models/staging/order_append.sql
@@ -3,4 +3,4 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','stg_orders')}} 
+select * from {{source('datafeed_shared_schema','stg_orders')}} where id in(1,2,3)
